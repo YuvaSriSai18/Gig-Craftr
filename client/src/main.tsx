10aad449af7d5b3913a5fed64bb596c7fc5bbd6a
Yuva Sri Sai <nanithota18102004@gmail.com>
2024-09-15 02:27:35 +0530

    commit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "@fontsource/poppins";
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Poppins", // Replace with your custom font family
+      "Arial", // Backup font
+      "sans-serif", // Fallback
+    ].join(","),
+  },
+  palette: {
+    primary: {
+      main: "#404020", // Replace with your primary color
+    },
+    secondary: {
+      main: "#dc004e", // Replace with your secondary color
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
+);
