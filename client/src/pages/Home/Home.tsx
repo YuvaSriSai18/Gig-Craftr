0dcfd72ea4217a10816128974b5dbca05d24fa79
Yuva Sri Sai <nanithota18102004@gmail.com>
2024-09-16 05:06:14 +0530

    added jobs and textWithImage

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,18 +1,18 @@
 // import React from 'react'
 import HeroSection from "../../components/Home/HeroSection";
 // import Carousel from "../../components/Home/Carousel";
-import Jobs from "../../components/Home/Jobs";
 import TextWithImage from "../../components/Home/TextWithImage";
+import Jobs from "../../components/Home/Jobs";
 import Footer from "../../components/Footer/Footer";
 import InfoSection from "../../components/Home/InfoSection";
-import CategoryGrid from '../../components/Home/loadcatogiry'
+import CategoryGrid from "../../components/Home/loadcatogiry";
 export default function Home() {
   return (
     <>
       <HeroSection />
       <InfoSection />
       <TextWithImage />
-      <CategoryGrid/>
+      <CategoryGrid />
       <Jobs />
       <Footer />
     </>
