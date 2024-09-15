872f55d082da5543d81e1ad5e6a1475e33f83399
Yuva Sri Sai <nanithota18102004@gmail.com>
2024-09-15 10:53:21 +0530

    created new files

---
+++ client/src/pages/FindFreelancers/FindFreelancers.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FindFreelancers() {
+  return (
+    <div>
+      FindFreelancers
+    </div>
+  )
+}
---
+++ client/src/pages/PostProject/PostProject.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PostProject() {
+  return (
+    <div>
+      Post Project
+    </div>
+  )
+}
